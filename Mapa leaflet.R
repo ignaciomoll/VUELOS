628ec7c8a97352a3ac35ab4091d9a217d9f1f948
ignacio gamez moll <ignacio@MacBook-Pro-de-ignacio.local>
2018-01-17 01:26:13 +0100

    Puedes crear una lista de mapas que despues llamas mas abajo cuando definas la funcción addProviderTiles(), nosotros solo necesitaremos el de eeuu

--- Mapa leaflet.R
+++ Mapa leaflet.R
@@ -16,3 +16,19 @@ map1 <- leaflet(width = 400, height = 200) %>%
                    lat = c(40.65236, 33.64044))
 
 map1
+
+
+# There are an enormous number of publically available tile layers for you to use.
+# You can check out Leaflet Providers for a pretty exhaustive list
+maptypes <- c("MapQuestOpen.Aerial",
+              "Stamen.TerrainBackground",
+              "Esri.WorldImagery",
+              "OpenStreetMap",
+              "Stamen.Watercolor")
+
+# Change the index to see what the different tile-sets look like:
+# Now we're into the magrittr formatting.  We're using "Provider" tiles here,
+# not the default (OpenStreetMap) Tiles:
+
+map <- leaflet() %>% 
+  addProviderTiles(maptypes[5])  # el 3 mola mucho!!!
