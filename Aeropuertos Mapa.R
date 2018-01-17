36c1a47300f01450b1f31959da3eb9f0d4bda824
ignacio gamez moll <ignacio@MacBook-Pro-de-ignacio.local>
2018-01-17 02:33:53 +0100

    Primer mapa realmente interactivo, la opcion popup te permite seleccionar los puntos y saber el nombre de los aeropuertos a su vez que cluster te permite agurpar y desagrupar a la vez, muy chulo de verdad.

--- Aeropuertos Mapa.R
+++ Aeropuertos Mapa.R
@@ -2,10 +2,32 @@
 # en shiny y por último en el proyecto final con el dataset correcto
 
 aeropuertos <- read.csv("airports.csv")
+summary(aeropuertos)
 # haciendo un brebe análisis de las variables, tenemos Iata_CODE y nombre del aeropuerto, (origen,destino),
-# estado, ciudad y coordenadas (latitud y longitud), no hay valores perdidos, podemos trabajar con el
+# estado, ciudad y coordenadas (latitud y longitud)
+
+# quitamos los valores perdidos NA, sólo hay tres en latitud y longitud
+aeropuertos <- na.omit(aeropuertos)
 
 plot(LATITUDE ~ LONGITUDE, 
      data = aeropuertos)
 # nos hacemos una idea de donde están los aeropuertos, los propios puntos han hecho el mapa de EEUU.
+
+
+# Agregamos la ventana emergente para poder hacer clic y ver qué sitio estoy buscando.
+
+map <- leaflet() %>% 
+  addProviderTiles("Esri.WorldImagery") %>% 
+  addMarkers(lat = aeropuertos$LATITUDE, 
+             lng = aeropuertos$LONGITUDE,
+             clusterOptions = markerClusterOptions(),
+             popup = as.character(aeropuertos$AIRPORT)) # te permite pulsar los puntos de mapa y te dice el nombre del aeropuerto
+
+map
+# aparecen aeropuertos, no sólo en EEUU sino también en Canada, por lo tanto es de Norte América entero, lo de los 
+# cluster sirve para ver el numero de aeropuertos en un áerea predeterminada, vemos como en EEUU hay 293 y el resto
+# hasta 319 están en Canda.
+# Puedes ir pulsando los cluster y se va acercando y dividiendo a su vez en más cluster hasta quedar un único aeropuerto
+
+
 
