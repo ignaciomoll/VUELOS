0e2cdc09c31bda5d819bc3c1bf03cddd379737e6
ignacio gamez moll <ignacio@MacBook-Pro-de-ignacio.local>
2018-01-17 01:08:08 +0100

    he puesto dos coordenadas para ver si salen señalizadas, y así es

--- Mapa leaflet.R
+++ Mapa leaflet.R
@@ -8,3 +8,11 @@ map <- leaflet(width = 400, height = 200) %>%
                    popup = "you are here")
 
 map
+# primer mapa creado con la libreria leaflet; voy a probar metiendo unas coordenadas de un aeropuerto al azar
+
+map1 <- leaflet(width = 400, height = 200) %>%
+        addTiles() %>%
+        addMarkers(lng = c(75.4404, -84.42694),
+                   lat = c(40.65236, 33.64044))
+
+map1
